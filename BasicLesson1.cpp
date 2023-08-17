3f0346ce13ceaf986753dc0667d1e4f987dbc08c
Breno da Silva Paz <56359183+BrenoPaz@users.noreply.github.com>
2023-08-17 15:12:06 -0300

    Update BasicLesson1.cpp

--- BasicLesson1.cpp
+++ BasicLesson1.cpp
@@ -5,6 +5,9 @@ using namespace std;
 // void - utilizado para retorno nulo
 
 int main(){   //função principal
-
+  cout << "Estrutura básica em C++" << endl;
+  cout << "Parabéns Zé" << endl;
+  
+  system("pause");//congela o programa e espera até que pressione qualquer tecla para pode pular.
   return 0;
 }
