786db7a669adbf01e2036fa0c43b14b8e3bb21ad
Breno da Silva Paz <56359183+BrenoPaz@users.noreply.github.com>
2023-08-17 16:25:07 -0300

    Create 3_ConstDefineVar.cpp

---
+++ 3_ConstDefineVar.cpp
@@ -0,0 +1,22 @@
+/*----------------------------------------------
+Declarações múltiplas de variáveos e defines.
+----------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+#define pi 3.1415 
+// Constante -> Valor fixo, não vai mudar de forma alguma. Por exemplo, pi = 3.1415.
+#define prof cout << "Breno\n\n";
+// Constante prof com o cout -> ; foi necessário no final, pois estávamos usando um cout.
+
+int main(){
+  int vidas = 3, tiros = 500, life = 100; // Podem ser declaradas individualmente ou juntas.
+
+  cout << pi << "\n\n";
+  canal
+
+
+
+  return 0
+}
