9b639debb5abbcef6a3598d9452ad686073ba871
Breno da Silva Paz <56359183+BrenoPaz@users.noreply.github.com>
2023-08-18 15:49:13 -0300

    Update 6_InvVar.cpp

--- 6_InvVar.cpp
+++ 6_InvVar.cpp
@@ -1,2 +1,17 @@
 // Inversão de valores de variáveis
 #include <iostream>
+using namespace std;
+
+int main(){
+  num = 10;
+  cout << num << "\n\n";
+
+  //método tradicional para inversão de valor-> num = num * -1;
+
+  cout << -num << "\n\n"; // inverte momentâneamente.
+
+  //forma simplificada:
+  num=-num;
+
+  return 0;
+}
